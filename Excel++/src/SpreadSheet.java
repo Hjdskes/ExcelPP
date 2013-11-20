ecbd311caad7ea3825d87cc5bbef675c62275597
Unia <jthidskes@outlook.com>
2013-11-20 14:32:31 +0100

    controleren of het voor iedereen werkt

--- Excel++/src/SpreadSheet.java
+++ Excel++/src/SpreadSheet.java
@@ -1,4 +1,7 @@
 
 public class SpreadSheet {
-
+	public static void main(String[] args) {
+		System.out.println("Als iedereen zijn eigen naam toevoegd kunnen we kijken of Git voor iedereen juist is ingesteld.");
+		System.out.println("Jente Hidskes");
+	}
 }
