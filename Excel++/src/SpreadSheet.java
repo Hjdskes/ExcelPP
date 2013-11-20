7e27837e1506c000cf5a209100e12f542fbae393
gfokkema <g.r.fokkema@student.tudelft.nl>
2013-11-20 14:33:42 +0100

    Naam toegevoegd

--- Excel++/src/SpreadSheet.java
+++ Excel++/src/SpreadSheet.java
@@ -3,5 +3,6 @@ public class SpreadSheet {
 	public static void main(String[] args) {
 		System.out.println("Als iedereen zijn eigen naam toevoegd kunnen we kijken of Git voor iedereen juist is ingesteld.");
 		System.out.println("Jente Hidskes");
+		System.out.println("Gerlof Fokkema");
 	}
 }
