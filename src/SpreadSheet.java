16d771d1ba603ecc2f67d8de25625b81a791ef93
Unia <jthidskes@outlook.com>
2013-11-20 18:37:08 +0100

    test

--- src/SpreadSheet.java
+++ src/SpreadSheet.java
@@ -2,5 +2,6 @@ public class SpreadSheet {
 	public static void main(String[] args) {
 		System.out.println("Voeg je naam toe om te testen.");
 		System.out.println("Gerlof");
+		System.out.println("Jente");
 	}
 }
