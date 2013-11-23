9230a0ab98588504a504c5d5a7e1007a68029f54
Skip Lentz <Skip@Skip-Lentzs-MacBook-Pro.local>
2013-11-23 17:05:20 +0100

    geen parser van w3c?

--- src/com/awesome/excelpp/xml/W3CParser.java
+++ src/com/awesome/excelpp/xml/W3CParser.java
@@ -2,6 +2,8 @@ package com.awesome.excelpp.xml;
 /*
  * Dave & Joris
  * Uitproberen van w3c parser.
+ * Ligt het aan mij of is er Ÿberhaupt geen parser van W3C?
+ * De parser in javax returnt een Document object, wat gebaseerd is op org.w3c.dom.Document
  */
 public class W3CParser {
 
