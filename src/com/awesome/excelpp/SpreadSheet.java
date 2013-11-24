ab9def31a4a5b02f408f521fc747ddf6f2f906c5
Joris Lamers <jclamers@tudelft.nl>
2013-11-24 19:57:56 +0100

    My try on a XML Parser
    Kijk en de console voor de output van de file. Dit moet genoeg zijn voor
    het overtuigen van de assistenten van het inladen van de file.

--- src/com/awesome/excelpp/SpreadSheet.java
+++ src/com/awesome/excelpp/SpreadSheet.java
@@ -1,10 +1,16 @@
 package com.awesome.excelpp;
 
-import com.awesome.excelpp.xml.*;
-import com.awesome.excelpp.gui.*;
+import com.awesome.excelpp.gui.MainScreen;
+
 
 public class SpreadSheet {
 	public static void main(String[] args) {
 		new MainScreen();
+		xmlParser();
+	}
+
+	private static void xmlParser() {
+		xmlParser.parse();
+		
 	}
 }
