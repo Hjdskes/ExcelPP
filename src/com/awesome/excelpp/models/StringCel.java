2c58217fd5c94f389656fc396eb231e4797438bb
Joris Lamers <jclamers@tudelft.nl>
2013-11-27 16:17:01 +0100

    Revert "Cell Update"
    This reverts commit 2588a987209af42a7891c51f21d76c2285bd17e0.

--- src/com/awesome/excelpp/models/StringCel.java
+++ src/com/awesome/excelpp/models/StringCel.java
@@ -1,10 +1,5 @@
 package com.awesome.excelpp.models;
 
 public class StringCel extends Cel {
-	private String content;
-	
-	public StringCel(int col, int rw, String cont) {
-		super(col, rw);
-		this.content = cont;
-	}
+
 }
