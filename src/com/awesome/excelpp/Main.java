f4cad28f4020678015aef107b95a061f905674d0
Unia <jthidskes@outlook.com>
2013-11-27 15:57:27 +0100

    SpreadSheet hernoemd naar Main

---
+++ src/com/awesome/excelpp/Main.java
@@ -0,0 +1,9 @@
+package com.awesome.excelpp;
+
+import com.awesome.excelpp.gui.GUI;
+
+public class Main {
+	public static void main(String[] args) {
+		new GUI();
+	}
+}
---
+++ src/com/awesome/excelpp/MainTest.java
@@ -0,0 +1,12 @@
+package com.awesome.excelpp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MainTest {
+	@Test
+	public void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+}
