b7937bd0b87fd6f1ff069110ccb8445941f4c0af
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2013-11-27 22:59:39 +0100

    SpreadSheetTest:	Added basic tests for SpreadSheet class

--- src/com/awesome/excelpp/models/SpreadSheetTest.java
+++ src/com/awesome/excelpp/models/SpreadSheetTest.java
@@ -2,10 +2,27 @@ package com.awesome.excelpp.models;
 
 public class SpreadSheetTest {
 	public static void main(String[] args) {
-		SpreadSheet sheet = new SpreadSheet();
+		String res1 = test(new SpreadSheet());
+		String res2 = test(new SpreadSheetHashMap());
+		
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+	
+	public static String test(SpreadSheet sheet) {
 		System.out.println("Filling sheet...");
+		long fillStart = System.currentTimeMillis();
 		sheet.fillSheet();
+		long fillEnd = System.currentTimeMillis();
+		
+		
 		System.out.println("Printing sheet...");
+		long printStart = System.currentTimeMillis();
 		System.out.println(sheet.toString());
+		long printEnd = System.currentTimeMillis();
+		
+		String res = "Time to fill: " + (fillEnd - fillStart) + " ms\n" +
+						"Time to print: " + (printEnd - printStart) + " ms";
+		return res;
 	}
 }
