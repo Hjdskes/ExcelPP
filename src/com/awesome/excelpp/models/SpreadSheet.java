396ee80a9bff53132c93adc7cedc7d139444efc2
Skip <skip_meesie@hotmail.com>
2013-11-27 16:45:44 +0100

    Changed to 2D-array, implemented setters and getters

--- src/com/awesome/excelpp/models/SpreadSheet.java
+++ src/com/awesome/excelpp/models/SpreadSheet.java
@@ -1,31 +1,30 @@
 package com.awesome.excelpp.models;
 
-import java.util.HashMap;
 import java.util.Observable;
 
 public class SpreadSheet extends Observable {
-	private HashMap<Integer, Cel> cells;
+	private Cel[][] cells;
+	private short numberOfRows;
 	private short numberOfCols;
 	
 	public SpreadSheet(){
-		cells = new HashMap<Integer, Cel>();
-	}
-	
-	public void addCol(){
-		numberOfCols--
-		numberOfCols+=2;
+		numberOfRows = 1000;
+		numberOfCols = 1000;
+		cells = new Cel[numberOfRows][numberOfCols];
 	}
 	
 	public void setCel(int row, int col, Cel c){
-		
+		if(row < numberOfRows - 1 && col < numberOfCols - 1){
+			cells[row][col] = c;
+		}
 	}
 	
 	public Cel getCel(int row, int col){
-		return cells.get(row*numberOfCols + col);
+		return cells[row][col];
 	}
 	
 	public String getValue(int row, int col){
-		return "";
+		return cells[row][col].getValue();
 	}
 }
 
