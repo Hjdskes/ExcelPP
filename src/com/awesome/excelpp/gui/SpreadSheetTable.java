ce32412cf5ce090df2c78e76f8eb8cc698c91ca9
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2013-12-01 01:42:48 +0100

    Deleted JTable CellTable, because we already extend it. Added test method to enable first link GUI <-> SpreadSheet

--- src/com/awesome/excelpp/gui/SpreadSheetTable.java
+++ src/com/awesome/excelpp/gui/SpreadSheetTable.java
@@ -2,20 +2,35 @@ package com.awesome.excelpp.gui;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+
+import com.awesome.excelpp.models.SpreadSheet;
+
 import java.awt.Color;
 
 public class SpreadSheetTable extends JTable {
 	private static final long serialVersionUID = 1L; // anders zeurt eclipse, maar waarom?
 	public static JScrollPane scrollPane;
-	public static JTable cellTable;
 
 	public SpreadSheetTable () {
-		JTable cellTable = new JTable ();
+		super(10, 10);
+		
 		Color backgroundColor = new Color(222, 222, 222);
 
-		cellTable.setFillsViewportHeight (true);
-		cellTable.setSelectionBackground (backgroundColor);
-		//cellTable.setOpaque(true); //MUST do this for background to show up. even verifiëren
-		JScrollPane scrollPane = new JScrollPane (cellTable);
+		setFillsViewportHeight (true);
+		setSelectionBackground (backgroundColor);
+		//setOpaque(true); //MUST do this for background to show up. even verifiëren
+		new JScrollPane (this);
+		
+		test();
+	}
+	
+	public void test() {
+		SpreadSheet sheet = new SpreadSheet();
+		sheet.fillSheetFormula();
+		for (int row = 0; row < 10; row++) {
+			for (int col = 0; col < 10; col++) {
+				this.setValueAt(sheet.getValue(row, col), row, col);
+			}
+		}
 	}
 }
