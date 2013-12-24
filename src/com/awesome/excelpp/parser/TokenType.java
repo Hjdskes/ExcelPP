339f62bd2ed4b335e0213131c153661d4b7de61e
Balletie <skip_meesie@hotmail.com>
2013-12-25 00:07:30 +0100

    added Cell tokentype

--- src/com/awesome/excelpp/parser/TokenType.java
+++ src/com/awesome/excelpp/parser/TokenType.java
@@ -2,6 +2,7 @@ package com.awesome.excelpp.parser;
 
 public enum TokenType {
 	NUMBER("[0-9]+"),			// (-)0-9
+	CELL("[a-zA-Z][0-9]+"),		// A-Z0-9
 	WORD("[a-zA-Z]+"),			// a-z, A-Z
 	PLUSMINUS("[+-]"),			// +, -
 	MULTDIV("[*/]"),			// *, /
