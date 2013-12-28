52fd547ae0bfdc55cff7cad085a9cb6bb5d23ce4
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2013-12-28 18:09:12 +0100

    added several testcases and changed doubles actual and excpected to class variables

--- src/com/awesome/excelpp/parser/ParserTest.java
+++ src/com/awesome/excelpp/parser/ParserTest.java
@@ -4,32 +4,79 @@ import com.awesome.excelpp.models.SpreadSheet;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class ParserTest {
-	@Test
-	public void test_number() {
-		double expected = 2.0;
-		double result = new Parser("=2").eval();
-		assertEquals(expected, result, .001);
-	}
+public class ParserTest {	
+	double expected, result;
 	
 	@Test
-	public void test_minus() {
-		double expected = 0.0;
-		double result = new Parser("=2-2").eval();
+	public void test_number() {
+		expected = 2.0;
+		result = new Parser("=2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = 450.257;
+		result = new Parser("=450.257").eval();
 		assertEquals(expected, result, .001);
 	}
 	
 	@Test
 	public void test_plus() {
-		double expected = 4.0;
-		double result = new Parser("=2+2").eval();
+		expected = 4.0;
+		result = new Parser("=2+2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = 28.0;
+		result = new Parser("=25.37+2.63").eval();
+		assertEquals(expected, result, .001);
+	}
+	
+	@Test
+	public void test_minus() {
+		expected = 0.0;
+		result = new Parser("=2-2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = -3.99;
+		result = new Parser("=2.38-6.37").eval();
+		assertEquals(expected, result, .001);
+	}
+	
+	@Test
+	public void test_unaryminus() {
+		expected = -4.0;
+		result = new Parser("=-2---2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = -.57;
+		result = new Parser("=2+-2.57").eval();
+		assertEquals(expected, result, .001);
+	}
+	
+	@Test
+	public void test_multiply() {
+		expected = 50.0;
+		result = new Parser("=25*2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = 5.0;
+		result = new Parser("=2*2.5").eval();
+		assertEquals(expected, result, .001);
+	}
+	
+	@Test
+	public void test_division() {
+		expected = 12.5;
+		result = new Parser("=25/2").eval();
+		assertEquals(expected, result, .001);
+		
+		expected = 40;
+		result = new Parser("=30/.75").eval();
 		assertEquals(expected, result, .001);
 	}
 	
 	@Test
 	public void test_nested_functions() {
-		double expected = 3.0;
-		double result = new Parser("=Add(1,2,Add(-3,4),Subtract(5,6))").eval();
+		expected = 3.0;
+		result = new Parser("=Add(1,2,Add(-3,4),Subtract(5,6))").eval();
 		assertEquals(expected, result, .001);
 	}
 	
@@ -37,8 +84,8 @@ public class ParserTest {
 	public void test_cell() {
 		SpreadSheet testSheet = new SpreadSheet();
 		testSheet.setValueAt("=4*5", 0, 2);
-		double expected = 16.0;
-		double result = new Parser("=-2*2+C1", testSheet).eval();
+		expected = 16.0;
+		result = new Parser("=-2*2+C1", testSheet).eval();
 		assertEquals(expected, result, .001);
 	}
 	
