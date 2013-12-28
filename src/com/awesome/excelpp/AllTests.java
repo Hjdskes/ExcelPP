5e26cf09a9c578bd9c7d63672895eae533178934
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2013-12-28 20:31:43 +0100

    added latest tests to AllTests: branch coverage 32.5%

--- src/com/awesome/excelpp/AllTests.java
+++ src/com/awesome/excelpp/AllTests.java
@@ -7,12 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.awesome.excelpp.models.CellTest;
 import com.awesome.excelpp.models.SpreadSheetTest;
 import com.awesome.excelpp.parser.LexerTest;
+import com.awesome.excelpp.parser.ParserTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ MainTest.class,
 				CellTest.class,
+				SpreadSheetTest.class,
 				LexerTest.class,
-				SpreadSheetTest.class })
+				ParserTest.class })
 public class AllTests {
 
 }
