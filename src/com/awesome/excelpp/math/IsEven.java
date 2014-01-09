533f37fb9955e865e0788ba031e89cddfd8573f9
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-01-09 16:09:52 +0100

    implemented formula's with a fixed number of arguments

--- src/com/awesome/excelpp/math/IsEven.java
+++ src/com/awesome/excelpp/math/IsEven.java
@@ -1,17 +1,18 @@
 package com.awesome.excelpp.math;
 
+import com.awesome.excelpp.math.exception.MathException;
+
 
 public class IsEven extends Formula {
-	public static double getValue(double input) {
+	@Override
+	public double getValue(double... numbers) throws MathException {
+		if (numbers.length > 1) {
+			throw new MathException();
+		}
+		
 		double res = 0.0;
-		if (input%2==0)
+		if (numbers[0] % 2 == 0)
 				res = 1.0;
 		return res;
-	}
-
-	@Override
-	public double getValue(double... numbers) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
