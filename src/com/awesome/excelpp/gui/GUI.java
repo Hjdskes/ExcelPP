df6cbc3e506b5a6bc2031a77c5b3e75c19491081
Unia <jthidskes@outlook.com>
2014-01-13 21:06:24 +0100

    fix filepath bij saven for real

--- src/com/awesome/excelpp/gui/GUI.java
+++ src/com/awesome/excelpp/gui/GUI.java
@@ -523,8 +523,8 @@ public class GUI extends JFrame implements ActionListener, KeyListener, WindowLi
 			int choice = fc.showSaveDialog(this);
 			if (choice == JFileChooser.APPROVE_OPTION) {
 				String fileString = fc.getSelectedFile().getPath();
-				fileString = fileString.substring(0, fileString.lastIndexOf("."));
-				fileString += ".xml";
+				if (!fileString.endsWith(".xml"))
+					fileString += ".xml";
 				file = new File(fileString);
 				table.setFile(file);
 				updateTabTitle(mainTabs.getSelectedIndex(), file.getName());
