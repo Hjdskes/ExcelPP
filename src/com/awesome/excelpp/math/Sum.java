66bcd5de1af54b11db744a2b7347cc8fbcd83ba5
Unia <jthidskes@outlook.com>
2014-01-19 21:32:30 +0100

    Add -> Sum

---
+++ src/com/awesome/excelpp/math/Sum.java
@@ -0,0 +1,18 @@
+package com.awesome.excelpp.math;
+
+import com.awesome.excelpp.math.exception.MathException;
+
+/**
+ * The add function returns the sum of all its arguments.
+ * @author Team Awesome
+ */
+public class Sum extends Formula {
+	@Override
+	public Double getValue(Object ... args) throws MathException  {
+		double sum = 0;
+		for (Object o : args) {
+			sum += getDouble(o);
+		}
+		return sum;
+	}
+}
