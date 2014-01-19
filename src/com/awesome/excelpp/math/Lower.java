c82b4e1e400ff379869b1bf6c75d75e711d73353
Balletie <skip_meesie@hotmail.com>
2014-01-19 15:26:10 +0100

    Ja ik weet het, sorry... Revert "Revert "Revert "String return"""
    This reverts commit a5546f30443655e5f7928bd230bdd746c2b3b347.

--- src/com/awesome/excelpp/math/Lower.java
+++ src/com/awesome/excelpp/math/Lower.java
@@ -1,17 +1,10 @@
 package com.awesome.excelpp.math;
 
-import com.awesome.excelpp.math.exception.MathException;
 
-
-public class Lower extends Formula {
-	public String getValue(Object... args) throws MathException {
-		if(args[0] instanceof String) {
-		String string = (String) args[0];
+public class Lower extends StringFormula {
+	public String getValue(String... vars) {
+		String string = vars[0];
 		string = string.toLowerCase();
 		return string;
-		}
-		else{
-			throw new MathException();
-		}
 	}
 }
