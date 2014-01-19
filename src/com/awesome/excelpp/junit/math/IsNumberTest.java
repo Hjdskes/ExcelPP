f2d088a92f237a84d148eb0a988d12257c2c3521
Unia <jthidskes@outlook.com>
2014-01-19 21:56:48 +0100

    fixed isNumerTest

--- src/com/awesome/excelpp/junit/math/IsNumberTest.java
+++ src/com/awesome/excelpp/junit/math/IsNumberTest.java
@@ -5,11 +5,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.awesome.excelpp.math.IsNumber;
+import com.awesome.excelpp.math.exception.MathException;
 
 public class IsNumberTest {
 	@Test
-	public void isNumber() {
-		assertEquals("true",new IsNumber().getValue("10"));
-		assertEquals("false",new IsNumber().getValue("a"));
+	public void isNumber() throws MathException {
+		assertEquals(true, new IsNumber().getValue(10));
+		assertEquals(false, new IsNumber().getValue("a"));
 	}
 }
