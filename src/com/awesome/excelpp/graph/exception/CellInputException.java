de6784af8bc8c9ac601ee4c5765d60670967ebc3
Unia <jthidskes@outlook.com>
2014-01-20 23:36:21 +0100

    ook hier JavaDoc toegevoegd

--- src/com/awesome/excelpp/graph/exception/CellInputException.java
+++ src/com/awesome/excelpp/graph/exception/CellInputException.java
@@ -1,5 +1,9 @@
 package com.awesome.excelpp.graph.exception;
 
+/**
+ * This exception is thrown when the selected <code>Cells</code> cannot be put into a chart.
+ * @author Team Awesome
+ */
 public class CellInputException extends Exception{
 	private static final long serialVersionUID = 1L;
 }
