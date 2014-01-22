6647f84d31c677f35c9e820420868cda630bc999
berndkr <bernd.kreynen@hotmail.com>
2014-01-22 16:13:00 +0100

    grootte helpDialog weer groter

--- src/com/awesome/excelpp/gui/GUI.java
+++ src/com/awesome/excelpp/gui/GUI.java
@@ -274,7 +274,7 @@ public class GUI extends JFrame implements ActionListener, KeyListener, WindowLi
 		helpTabbedPane.addTab("About", aboutLabel);
 
 		helpDialog.add(helpPanel);
-		helpDialog.setSize(410, 458);
+		helpDialog.setSize(410, 586);
 		helpDialog.setIconImage(mainImage);
 		helpDialog.setResizable(false);
 		helpDialog.setLocation ((screenWidth / 2) - (helpPanel.getPreferredSize().width / 2), (screenHeight / 2) - (helpPanel.getPreferredSize().height / 2)); //center in het midden
