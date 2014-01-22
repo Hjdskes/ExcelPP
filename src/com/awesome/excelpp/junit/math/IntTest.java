2ec813bac8a34e99cba494afd4a4d74699eb341d
dave <d.grund@student.tudelft.nl>
2014-01-22 17:02:06 +0100

    3 nieuwe tests
    Add
    And
    Int

--- src/com/awesome/excelpp/junit/math/IntTest.java
+++ src/com/awesome/excelpp/junit/math/IntTest.java
@@ -10,8 +10,12 @@ import com.awesome.excelpp.math.exception.MathException;
 public class IntTest {
 	@Test
 	public void Int() throws MathException {
-		assertEquals("1", new Int().getValue("1.0"));
-		assertEquals("2", new Int().getValue("2"));
+		assertEquals(1, new Int().getValue(1.0), .001);
+	}
+	
+	@Test(expected = MathException.class)
+	public void IntMathException() throws MathException {
+		assertEquals(1, new Int().getValue(1.0, 2.0), .001);
 	}
 	
 	@Test(expected = MathException.class)
